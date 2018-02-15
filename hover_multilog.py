3d0529f28386f75dea1b09343597592f36491605
David Schneider <simplexsigil@gmail.com>
2018-02-15 18:39:06 +0100

    Changed the logger to specifically listen for added parameters in firmware

--- hover_multilog.py
+++ hover_multilog.py
@@ -48,7 +48,7 @@ URI = 'radio:' + address
 log_base_path = "logdata/"
 log_file_prefix = "CF_{address}_{time}_".format(address=address.strip("/").replace("/","-"), time=strftime("%Y-%m-%d-%H-%M-%S", time.gmtime()))
 
-small_log = False
+small_log = True
 
 height = 1
 log_time = 50
@@ -83,28 +83,17 @@ def main():
     else:
 
         if (small_log):
-            logConfig_stab = LogConfig(name='Stabilizer', period_in_ms=log_intervall)
+            logConfig_stab = LogConfig(name='AttitudeBeforePosCtrl', period_in_ms=log_intervall)
+            logConfig_stab.add_variable('bposatt.roll', 'float')
+            logConfig_stab.add_variable('bposatt.pitch', 'float')
+            logConfig_stab.add_variable('bposatt.yaw', 'float')
+            logConfig_stab.add_variable('bposatt.actuatorThrust', 'float')
 
-            logConfig_stab.add_variable('stabilizer.roll', 'float')
-            logConfig_stab.add_variable('stabilizer.pitch', 'float')
-            logConfig_stab.add_variable('stabilizer.yaw', 'float')
-            logConfig_stab.add_variable('stabilizer.thrust', 'float')
-
-            logConfig_state = LogConfig(name='State', period_in_ms=log_intervall)
-            logConfig_state.add_variable('stateEstimate.x', 'float')
-            logConfig_state.add_variable('stateEstimate.y', 'float')
-            logConfig_state.add_variable('stateEstimate.z', 'float')
-
-            logConfig_target = LogConfig(name='ControlTarget', period_in_ms=log_intervall)
-            logConfig_target.add_variable('ctrltarget.roll', 'float')
-            logConfig_target.add_variable('ctrltarget.pitch', 'float')
-            logConfig_target.add_variable('ctrltarget.yaw', 'float')
-
-            logConfig_mpow = LogConfig(name='MotorPower', period_in_ms=log_intervall)
-            logConfig_mpow.add_variable('motor.m1', 'int32_t')
-            logConfig_mpow.add_variable('motor.m2', 'int32_t')
-            logConfig_mpow.add_variable('motor.m3', 'int32_t')
-            logConfig_mpow.add_variable('motor.m4', 'int32_t')  
+            logConfig_state = LogConfig(name='AttitudeAfterPosCtrl', period_in_ms=log_intervall)
+            logConfig_state.add_variable('aposatt.roll', 'float')
+            logConfig_state.add_variable('aposatt.pitch', 'float')
+            logConfig_state.add_variable('aposatt.yaw', 'float')
+            logConfig_state.add_variable('aposatt.actuatorThrust', 'float')  
 
         logConfig_all = LogConfig(name='PositionLog', period_in_ms=log_intervall)
         logConfig_all.add_variable('stateEstimate.x', 'float')
@@ -118,12 +107,8 @@ def main():
             if (small_log):
                 logger_stab = SyncLogger(scf, logConfig_stab)
                 logger_state = SyncLogger(scf, logConfig_state)
-                logger_target = SyncLogger(scf, logConfig_target)
-                logger_mpow = SyncLogger(scf, logConfig_mpow)
                 logger_stab.connect()
                 logger_state.connect()
-                logger_target.connect()
-                logger_mpow.connect()
             
             logger_all = SyncLogger(scf, logConfig_all)
             logger_all.connect()
@@ -178,17 +163,8 @@ def main():
                     log_count = write_out_log(logger_state, f, log_start, log_end)
                     print_message("logged state")
 
-                with open(log_file_name(logConfig_target), "w") as f:
-                    log_count = write_out_log(logger_target, f, log_start, log_end)
-                    print_message("logged control target")
-
-                with open(log_file_name(logConfig_mpow), "w") as f:
-                    log_count = write_out_log(logger_mpow, f, log_start, log_end)
-                    print_message("logged motor power")
                 logger_stab.disconnect()
                 logger_state.disconnect()
-                logger_target.disconnect()
-                logger_mpow.disconnect()
         
             with open(log_file_name(logConfig_all), "w") as f:
                 log_count = write_out_log(logger_all, f, log_start, log_end)
