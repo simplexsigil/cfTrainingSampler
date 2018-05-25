0b7dacb20180923ca6475a9f2afa8062039f1a23
Tino Fuhrmann <tino-fuhrmann@web.de>
2018-05-25 04:21:35 +0200

    Updated README & logging_api interface documentation

--- framework/logging_api/logging_api/interface.py
+++ framework/logging_api/logging_api/interface.py
@@ -18,14 +18,21 @@ class LoggingAPI:
     def __init__(self, zmqContext, zmqServerAddress, port):
         self.connection = LoggingConnection(zmqContext, zmqServerAddress, port)
 
-    """
-        Returns true if command was received
-    """
     def startLogging(self):
+        """
+            @return true if command was received
+        """
         return self.connection.sendCommand({ "action": "start_logging"})["success"]
 
     def stopLogging(self):
+        """
+            @return true if command was received and logging stopped
+        """
         return self.connection.sendCommand({ "action": "stop_logging"})["success"]        
 
     def getInfo(self):
+        """
+            Currently does not return any interesting information.
+            @return { success: boolean, msg: string }
+        """
         return self.connection.sendCommand({"action": "info"});
